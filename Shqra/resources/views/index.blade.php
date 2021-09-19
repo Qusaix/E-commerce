ef4e886f7feaba6f78db9d5f9019fbe216397c6a
Qusaix <qusaiznemat6@gmail.com>
2021-09-19 11:22:12 +0300

    fixs

--- Shqra/resources/views/index.blade.php
+++ Shqra/resources/views/index.blade.php
@@ -883,31 +883,6 @@
 	
 				<div class="owl-carousel owl-theme banner_2_slider">
 	
-					@foreach ($slider as $p )
-					<div class="owl-item">
-						<div class="banner_2_item">
-							<div class="container fill_height">
-								<div class="row fill_height">
-									<div class="col-lg-4 col-md-6 fill_height">
-										<div class="banner_2_content">
-											{{-- <div class="banner_2_category">{{$p->categore->Title}}</div>  --}}
-											<div class="banner_2_title">{{$p->Title}}</div>
-											<div class="banner_2_text">{{$p->description}}</div>
-											{{-- <div class="rating_r rating_r_4 banner_2_rating"><i></i><i></i><i></i><i></i><i></i></div> --}}
-											<div class="button banner_2_button"><a href="{{route('products',$p->id)}}">Explore</a></div>
-										</div>
-										
-									</div>
-									<div class="col-lg-8 col-md-6 fill_height">
-										<div class="banner_2_image_container">
-											<div class="banner_2_image"><img src="{{$p->image}}" alt=""></div>
-										</div>
-									</div>
-								</div>
-							</div>			
-						</div>
-					</div>
-					@endforeach
 					<!-- Banner 2 Slider Item -->
 				
 	
