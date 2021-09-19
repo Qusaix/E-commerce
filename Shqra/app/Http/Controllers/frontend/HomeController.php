8aeac1dac36aa9e660e917dd5ee06c73d10152b1
Qusaix <qusaiznemat6@gmail.com>
2021-09-19 10:52:40 +0300

    small fix

--- Shqra/app/Http/Controllers/frontend/HomeController.php
+++ Shqra/app/Http/Controllers/frontend/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\categores;
+use App\Categores;
 use App\Ads;
 use App\Featured;
 use App\Countdown;
